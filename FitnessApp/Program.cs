2c93f86b70dad80530a576c9f79b94a24422e670
Enzo35771 <st10035771@vcconnect.edu.za>
2024-03-07 16:20:24 +0200

    provided base to work with

--- FitnessApp/Program.cs
+++ FitnessApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FitnessApp
+﻿using System;
+
+namespace FitnessApp
 {
     internal class Program
     {
@@ -6,5 +8,41 @@
         {
             
         }
+        public void addExercise()
+        {
+            Console.WriteLine(" what exercise are you doing?");
+            String exercise = Console.ReadLine();
+            Console.WriteLine("how long is the exercise?");
+            int duration = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("how many calories will you burn?");
+            int calories = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void menu()
+        {
+            Console.WriteLine("1. Add Exercise");
+            Console.WriteLine("2. View Summary");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Enter your choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    addExercise();
+                    break;
+                case 2:
+                    //viewExercise();
+                    break;
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+        }
+
     }
 }
